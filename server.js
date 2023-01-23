bad6f31dc98d5c8aeed956caa876c3a649c387e5
Matthew C <cnymhw86@gmail.com>
2023-01-23 15:12:34 -0500

    added first prompt

--- server.js
+++ server.js
@@ -1,2 +1,23 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // TODO: Add MySQL password here
+      password: '',
+      database: 'movies_db'
+    },
+console.log(`Connected to the mainframe.`)
+);
+
+const firstPrompt = [ 
+    {
+        type: 'list',
+        name: 'task',
+        message: 'What would you like to do?',
+        choices: ['view Employees', 'View Employees by Department', 'Add an Employee', 'Remove an Employee', 'Update an Employee Role', 'Add a New Role', 'End'],
+        
+    }]
