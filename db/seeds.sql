e835989a3fffd56dc9148dd6c34471c848eca85d
Matthew C <cnymhw86@gmail.com>
2023-01-23 17:31:09 -0500

    completed seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,22 +2,32 @@ USE employeeDB;
 
 INSERT INTO departments (name)
     VALUES 
-    ('human resouces'),
-    ('accounting'),
-    ('sales'),
-    ('marketing');
+    ('Sales'),
+    ('Engineering'),
+    ('Finance'),
+    ('Legal');
 
 INSERT INTO roles (department_id, title, salary)
     VALUES
-    (1, 'Recruiter', 80000),
-    (2, 'Accountant', 90000),
-    (3, 'Sales Representative', 60000),
-    (3, 'Director of Sales' 100000),
-    (4, 'Marketer' 60000);
+    (1, 'Sales Lead', 100000),
+    (1, 'Salesperson', 80000),
+    (2, 'Lead Engineer', 150000),
+    (2, 'Software Engineer' 120000),
+    (3, 'Account Manager' 160000);
+    (3, 'Accountant' 125000);
+    (4, 'Legal Team Lead' 250000);
+    (4, 'Lawyer' 190000);
 
-INSERT INTO roles (manager_id, first_name, last_name, role_id)
+INSERT INTO roles (manager_id, role_id, first_name, last_name)
     VALUES
-    
+    (null, 1, 'John', 'Doe');
+    (1, 2, 'Mike', 'Chan');
+    (null, 3, 'Ashley', 'Rodriguez');
+    (3, 4, 'Kevin', 'Tupik');
+    (null, 5, 'Kunal', 'Singh');
+    (5, 6 'Malia', 'Brown');
+    (null, 7, 'Sarah', 'Lourd');
+    (7, 8, 'Tom', 'Allen');
 
     
 
